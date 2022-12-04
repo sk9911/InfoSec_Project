72d7d55f9f87913a7524be7e146d52ba7886fd19
sk9911 <shreyas2kshirsagar@gmail.com>
2022-12-04 17:23:47 +0530

    File struct

--- RSAScripts/keyGen.py
+++ RSAScripts/keyGen.py
@@ -8,6 +8,6 @@ file_out.write(private_key)
 file_out.close()
 
 public_key = key.publickey().export_key()
-file_out = open("receiver.pem", "wb")
+file_out = open("public.pem", "wb")
 file_out.write(public_key)
 file_out.close()
--- RSAScripts/verify.py
+++ RSAScripts/verify.py
@@ -2,7 +2,7 @@ from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 
-key = RSA.import_key(open('receiver.pem').read())
+key = RSA.import_key(open('public.pem').read())
 
 file_in = open('message.txt', 'rb')
 message = file_in.read()
